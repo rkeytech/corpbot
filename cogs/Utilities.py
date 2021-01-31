60c8d776c69d842854d5f828c4862632946a876a
rkeytech <rkeytech@protonmail.com>
2021-01-31 15:56:05 +0000

    giveaway cmd

--- cogs/Utilities.py
+++ cogs/Utilities.py
@@ -1,10 +1,16 @@
 import discord
 from discord.ext import commands
+import csv
 
 class Utilities(commands.Cog):
     def __init__(self, client):
         self.client = client
-
+    
+    def read_csv(self, filename):
+        with open(filename, newline='') as f:
+            rdr = csv.reader(f, delimiter=',')
+            for row in rdr:
+                print(row)
     # Events
     # Create events with @commands.Cog.listener()
 
@@ -12,7 +18,8 @@ class Utilities(commands.Cog):
     # Create commands with @commands.command()
     @commands.command()
     async def giveaway(self, ctx):
-        pass
+        self.read_csv('giveaways.csv')
+        
 
 
 def setup(client):
