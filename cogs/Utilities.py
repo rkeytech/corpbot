40a4f2ccbacdadff2bfdb1c6e38fb5fd008e2e6f
zacharatos <rkeytech@protonmail.com>
2021-02-03 07:00:16 +0200

    giveaway modified

--- cogs/Utilities.py
+++ cogs/Utilities.py
@@ -24,47 +24,43 @@ class Utilities(commands.Cog):
     @commands.command()
     async def giveaway(self, ctx, name='Just'):
         guild = self.client.guilds[0]
-        cat = discord.utils.get(guild.categories, name='giveaways')
+        cat = discord.utils.get(guild.categories, name='Giveaways')
         member = ctx.message.author
         mbed = discord.Embed(
-            name=f"{name} GIVEAWAY",
+            name=f"{name} GIVEAWAY".upper(),
             description=f"This is a {name} giveaway so we can share with you \
                 something back, for all your troubles and for being \
-                    nice and active pilots.",
-            color=discord.Colour.dark_yellow()
+                nice and active pilots.",
+            color=discord.Colour.dark_gold()
         )
         mbed.set_author(name=member.nick, icon_url=member.avatar_url)
         mbed.add_field(
-            name='__Participate__',
+            name='__How To participate__',
             value="In order to participate in our giveaway the only thing you \
                 have to do is to react to this message. You can react with \
                 any emoticon you like. It doesn't matter. Or doed it? :)",
             inline=False
         )
-        perm = discord.Permissions.none()
-        perm.update({
-            'add_reactions': True,
-            'read_message_history': True,
-            'view_channel': True
-        })
+        mbed.set_footer(text=f"code:{member.id}")
+
         # Processing the giveaway
-        g_channel = await cat.create_text_channel(
-            name=f"{name} giveaway", overwrites=perm
-        )
+        g_channel = await cat.create_text_channel(name=f"{name} giveaway")
+        await g_channel.set_permissions(member, send_messages=True)
 
         msg = await g_channel.send(embed=mbed)
         await msg.pin()
 
     @commands.command()
-    async def end_giveaway(self, ctx, loot=None):
+    async def endg(self, ctx, loot=None):
         if ctx.message.channel.category.name == 'Giveaways':
             giveaway_items = ''
-            if not loot:
+            if loot:
                 giveaway_items = self.read_csv(f'./assets/{loot}.csv')
             pilots = set()
             pinned = await ctx.message.channel.pins()
             msg = await ctx.message.channel.fetch_message(pinned[0].id)
-            if msg.author.id == ctx.message.author.id:
+            owner_id = msg.embeds[-1].footer.text.split(':').strip()
+            if owner_id == str(ctx.message.author.id):
                 for reaction in msg.reactions:
                     users = await reaction.users().flatten()
                     pilots.update(users)
@@ -75,12 +71,12 @@ class Utilities(commands.Cog):
                     w_item = 'this giveaway'
                 await ctx.message.channel.send(
                     f"_**{winner.nick}**_ _is the winner for_ \
-                        _**{w_item}**_ _!!_"
+                    _**{w_item}**_ _!!_"
                 )
                 await winner.send(
                     f"You are the winner of the _**\
-                        {ctx.message.channel.name.split('-')[:-1]}\
-                        **_ giveaway! Congratulation!!"
+                    {ctx.message.channel.name.split('-')[0]}\
+                    **_ giveaway! Congratulation!!"
                 )
             else:
                 await ctx.message.channel.send(
